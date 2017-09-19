1a31fb93f61a751804caab934b73cf3c364f9ed3
FABIAN MARESGROUP <FABIAN MARESGROUP@DESKTOP-2ML40U1>
2017-09-19 17:52:30 -0500

    rep creation

---
+++ Source/MVCWebshop/MVCWebshop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWebshop.WijmoMvcApplication" Language="C#" %>
---
+++ Source/MVCWebshop/MVCWebshop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCWebshop.Startup))]
+namespace MVCWebshop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
